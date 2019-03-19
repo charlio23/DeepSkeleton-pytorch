9ce4a1124d3047fd2c3e2b573b9da9483d34dd5f
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-03-19 10:47:56 -0400

    parameter tunning

--- finetune.py
+++ finetune.py
@@ -9,7 +9,7 @@ import numpy as np
 print("Importing datasets...")
 
 rootDir = "SK-LARGE/"
-trainListPath = "SK-LARGE/aug_data/train_pair.lst"
+trainListPath = "train_pair_val.lst"
 
 trainDS = SKLARGE(rootDir, trainListPath)
 train_data = DataLoader(trainDS, shuffle=True, batch_size=1, num_workers=4)
@@ -20,7 +20,6 @@ eval_data = DataLoader(evalDS, shuffle=False, batch_size=1, num_workers=4)
 print("Initializing network...")
 
 modelPath = "model/vgg16.pth"
-nnet = torch.nn.DataParallel(initialize_fsds(modelPath)).cuda()
 
 print("Defining hyperparameters...")
 
@@ -36,6 +35,7 @@ results = []
 for learningRate in learningRates:
     for p in ps:
         try:
+            nnet = torch.nn.DataParallel(initialize_fsds(modelPath)).cuda()
             nnet = train(nnet, train_data, p, learningRate, 3)
             loss = evaluate(nnet, eval_data)
             results.append("lr: " + str(learningRate) + ", p: " + str(p) + "loss: " + str(loss))
