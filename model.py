d976a48cf954b9c8c1c4c5f55bedc85442903341
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-03-21 09:38:37 -0400

    changed learning strat

--- model.py
+++ model.py
@@ -117,8 +117,8 @@ class FSDS(torch.nn.Module):
 
         self.fuseScale3 = torch.nn.Conv2d(in_channels=2, out_channels=1,
             kernel_size=1, stride=1, padding=0)
-       
-       self.fuseScale4 = torch.nn.Conv2d(in_channels=1, out_channels=1,
+
+        self.fuseScale4 = torch.nn.Conv2d(in_channels=1, out_channels=1,
             kernel_size=1, stride=1, padding=0)
 
         self.softmax = torch.nn.Softmax(dim=1)
