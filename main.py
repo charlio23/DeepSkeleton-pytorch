7291425ea8bfffe596d2968a225a030a7cb323a1
charlio23 <carlesbalsellsrodas@hotmail.com>
2019-03-21 00:04:10 -0400

    changed learning rate

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ optimizer = torch.optim.SGD([
 ], lr=learningRate, momentum=momentum, weight_decay=weightDecay)
 
 # Learning rate scheduler.
-lr_schd = lr_scheduler.StepLR(optimizer, step_size=1e5, gamma=0.1)
+lr_schd = lr_scheduler.StepLR(optimizer, step_size=3e4, gamma=0.1)
 
 def balanced_cross_entropy(input, target):
     #weights
